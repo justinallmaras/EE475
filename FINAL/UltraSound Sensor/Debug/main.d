3190fc2f62a76a734b8c55463cfc058a51d90ce1
Justin Allmaras <justin.allmaras@gmail.com>
2017-02-18 16:21:50 -0800

    US sensor working

--- FINAL/UltraSound Sensor/Debug/main.d
+++ FINAL/UltraSound Sensor/Debug/main.d
@@ -1,15 +1,23 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: /Applications/ti/ccsv7/ccs_base/msp430/include/msp430.h
-main.obj: /Applications/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h
-main.obj: /Applications/ti/ccsv7/ccs_base/msp430/include/in430.h
-main.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h
-main.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdint.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdarg.h
 
 ../main.c: 
-/Applications/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
-/Applications/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h: 
-/Applications/ti/ccsv7/ccs_base/msp430/include/in430.h: 
-/Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h: 
-/Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdint.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdarg.h: 
--- FINAL/UltraSound Sensor/main.c
+++ FINAL/UltraSound Sensor/main.c
@@ -1,108 +1,91 @@
-#include <msp430.h> 
+#include <intrinsics.h>
+#include <stdint.h>
+#include <msp430.h>
+#include <stdio.h>
 
-
-
-
-
-
+//reminder turn off additional clocks to reduce power consumption
 #define TRIGGER_PIN     BIT4
 #define ECHO_PIN        BIT3
-#define MEAS_INTERVAL   60000
-
+#define LED_PIN         BIT3
+#define MEAS_INTERVAL  60000
+#define DISTANCE_CONST 58
 unsigned int up_counter;
-unsigned int distance_cm;
+unsigned int distance;
 
-
-/* Timer A0 Capture Interrupt routine
- P1.1 (echo) causes this routine to be called */
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void TIMER0_A0_ISR(void) {
-    if (TA0CCTL2 & CCI) {          // Raising edge
-
-        up_counter = TA0CCR2;   // Copy counter to variable
-
-    } else {                    // Falling edge
-
+/* Timer A0 Capture Interrupt routine P1.3 (echo) causes this routine to be called
+ */
+#pragma vector = TIMER0_A1_VECTOR
+__interrupt void TIMER0_A1_ISR(void) {
+    if (TA0CCTL2 & CCI) {           // Raising edge
+        up_counter = TA0CCR2;       // Copy counter to variable
+        P2OUT &= ~LED_PIN;
+    } else {                        // Falling edge
         // Formula: Distance in cm = (Time in uSec)/58
-        distance_cm = (TA0CCR2 - up_counter)/58;
+        distance = (TA0CCR2 - up_counter) / DISTANCE_CONST;
+        P2OUT |= LED_PIN;
+        printf("distance: %d\n", distance);
     }
-    TA0CTL &= ~TAIFG;           // Clear interrupt flag - handled
+
+
+    //TA0CTL &= ~TAIFG;           // Clear interrupt flag - handled
+    TA0IV = 0;
+    _BIS_SR_IRQ(GIE);
+}
+
+/* Triggers the measurement on the ultrasonic sensor using 10us pulse on P1.4
+ */
+void triggerMeas(void) {
+    // start trigger
+    P1OUT |= TRIGGER_PIN;
+
+    __delay_cycles(10);
+
+    // end trigger
+    P1OUT &= ~TRIGGER_PIN;
 }
 
 
 
-
-
-
-
-
-/*
- * main.c
- */
-
+// Initializes the pins and configures timer TA0 to capture echo pulses
 void usSensorInit(void) {
-
     // initialize trigger pin low to start
     P1DIR |= TRIGGER_PIN;
     P1OUT &= ~TRIGGER_PIN;
-
 
     // initialize echo pin to capture input
     P1DIR &= ~ECHO_PIN;
     P1SEL |= ECHO_PIN;
 
+    // Timer A0 configuration to read the echo signal
+    TA0CTL |= TASSEL_2 + ID_0 + MC_2 + TACLR;
 
-    // initialize TA0 to capture in CCR2 on both edges
+    // Timer TA0.2 control configuration
     TA0CCTL2 |= CM_3 + SCS + CCIS_0 + CAP + CCIE;
 
-
-    // initialize TA0 to compare CCR0
-    TA0CTL |= TASSEL_2 + MC_2 + ID_0;
-
-
-    //global interrupt
+    // Global interrupt disable
     _BIS_SR(GIE);
-
-
 }
 
-void triggerMeas(void) {
-
-    // start trigger
-    P1OUT ^= TRIGGER_PIN;
-
-    // wait for 10us for trigger pulse
-    __delay_cycles(10);
-
-    // end trigger
-    P1OUT ^= TRIGGER_PIN;
-
-}
-
-
-void usSensorMain(void) {
-    usSensorInit();
-
-    for(;;) {
-        triggerMeas();
-
-        // Adjust to fit measurement cycles
-        __delay_cycles(MEAS_INTERVAL);
-    }
-
-}
 
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;
 
-    usSensorMain();
+    // tester led
+    P2DIR |= LED_PIN;
+    P2OUT &= ~LED_PIN;
 
+    // initialize ultrasonic sensors
+    usSensorInit();
 
+    for(;;){
+        //send trigger
+        triggerMeas();
 
-
-
-
+        // wait measurement interval
+        __delay_cycles(60000);
+    }
     return 0;
 }
 
